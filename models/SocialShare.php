5fb41a90635c8f7527ca71b078b477928a12a9f0
charlie <jhdhj999@gmail.com>
2025-03-27 23:48:52 +0600

    Social media sharing model

---
+++ models/SocialShare.php
@@ -0,0 +1,41 @@
+<?php
+class SocialShare {
+    private $facebook_app_id = 'YOUR_FACEBOOK_APP_ID';
+    private $facebook_app_secret = 'YOUR_FACEBOOK_APP_SECRET';
+
+    // Get FB login url
+   public function getFacebookLoginUrl() {
+        $redirect_uri = 'http://yourdomain.com/fb-callback.php';
+        
+        $url = 'https://www.facebook.com/v12.0/dialog/oauth?' . http_build_query([
+            'client_id' => $this->facebook_app_id,
+            'redirect_uri' => $redirect_uri,
+            'state' => bin2hex(random_bytes(16)),
+            'scope' => 'public_profile,manage_pages,publish_pages'
+        ]);
+        
+        return $url;
+    }
+
+    // Post
+    public function postToFacebook($access_token, $message) {
+        $url = "https://graph.facebook.com/me/feed";
+        
+        $data = [
+            'message' => $message,
+            'access_token' => $access_token
+        ];
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return json_decode($response, true);
+    }
+}
+?>
